f4341648f0f58702ea97d03fe73c8874f7f1a0b3
Kishore KS <kishore@Kishores-MacBook-Pro.local>
2019-10-07 01:44:15 +0530

    Added Appointment Module in Doctors_Profile

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,14 +1,35 @@
 INSERT INTO pharmacy(name,city) VALUES ('Costco','Fremont');
-INSERT INTO pharmacy(name,city) VALUES ('Walgreen','New York');
-INSERT INTO pharmacy(name,city) VALUES ('CVS','Washington');
-INSERT INTO pharmacy(name,city) VALUES ('Walmart','Chicago');
+INSERT INTO pharmacy(name,city) VALUES ('Walgreen','Fremont');
+INSERT INTO pharmacy(name,city) VALUES ('CVS','Fremont');
+INSERT INTO pharmacy(name,city) VALUES ('Walmart','Fremont');
 
 
-INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Ranjani', 'Physician', 'Dermatology');
-INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Kannan', 'Surgeon', 'Cardiology');
-INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Karnan', 'Surgeon', 'Neurology');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Adam', 'Physician', 'Dermatologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Michael', 'Physician', 'Psychiatrist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.John', 'Physician', 'General Pediatrician');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Natasha', 'Physician', 'General Physician');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Robin', 'Physician', 'Gynecologists');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Aasha', 'Surgeon', 'Dermatologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Christopher', 'Surgeon', 'Cardiologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Donald', 'Surgeon', 'Neurologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Krone', 'Surgeon', 'Gastroentrologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Dwayne', 'Surgeon', 'Ophthalmologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Jhonson', 'Surgeon', 'Pathologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Jack', 'Surgeon', 'Radiologist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Tom', 'Surgeon', 'Orthopedist');
+INSERT INTO doctorsprofile(name,type,speciality) VALUES ('Dr.Akon', 'Surgeon', 'Dentist');
 
 
-INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('JOHN', 'Male', '45', 'California', 'TUBERCULOSIS', '10/09/2019', '3/10/2019');
 
-INSERT INTO appointment(name,age,gender,address,contact,doctor,disease,date,time) VALUES('Adam', '26', 'Male', 'Fremont,California', '+1203456732', 'Dr.Kannan', 'Body-Check up','20/10/2019', '10:00 AM');
+
+INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('John', 'Male', '45', 'California', 'TUBERCULOSIS', '10/09/2019', '3/10/2019');
+INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('Swagger', 'Male', '55', 'Washington', 'JAUNDICE', '11/09/2019', '23/09/2019');
+INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('Donald', 'Male', '38', 'Chicago', 'MALARIA', '21/09/2019', '2/10/2019');
+INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('Alexa', 'Female', '40', 'Columbia', 'Arthiritis', '13/09/2019', '30/09/2019');
+INSERT INTO patientsrecord(name,gender,age,address,disease,admission,discharge) VALUES('Nikki Bella', 'Female', '25', 'Oak Land', 'Spine Injury', '1/09/2019', '6/10/2019');
+
+
+
+INSERT INTO appointment(name,age,gender,address,contact,doctor,disease,date,time) VALUES('Morrison', '26', 'Male', 'Fremont,California', '+1203456732', 'Dr.Natasha', 'Body-Check up','20/10/2019', '10:00 AM');
+INSERT INTO appointment(name,age,gender,address,contact,doctor,disease,date,time) VALUES('John', '44', 'Male', 'Santa Clara,California', '+1423203452', 'Dr.Michael', 'Counselling','12/10/2019', '03:00 PM');
+INSERT INTO appointment(name,age,gender,address,contact,doctor,disease,date,time) VALUES('Alberto', '54', 'Male', 'Austin,Texas', '+1839493222', 'Dr.Adam', 'Allergy','25/10/2019', '7:30 PM');
